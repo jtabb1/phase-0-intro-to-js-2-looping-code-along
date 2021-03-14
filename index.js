f63b612a91fbf11dc26a735617b2adf552f8ceec
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-03-13 22:24:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(givers, occasion) {
+    let returnArray = [];
+    for(let i = 0; i < givers.length; i++) {
+        returnArray[i] = `Thank you, ${givers[i]}, for the wonderful ${occasion} gift!`
+    }
+    return returnArray;
+}
+
+// 
+
+function countDown(fromNum) {
+    for(let i = fromNum; i > -1; i--) {
+        console.log(i);
+    }
+}
+
+// countDown(10);
